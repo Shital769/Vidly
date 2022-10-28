ae6587292fece1623e604becb778b0e9d6991fa3
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-10-28 15:15:31 +1100

    few things such as routing parameters, query string, redirect, not found(404) pages and nested routing has been done

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Table from "./common/table";
 import Like from "./common/like";
 
 class MoviesTable extends Component {
   columns = [
-    { path: "title", label: "Title" },
+    //linking navbar to the movieform as content
+    {
+      path: "title",
+      label: "Title",
+      content: (movie) => (
+        <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+      ),
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
