9c4b4b4e3ce445e4cb424c0835d52d1952e2bc9e
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-11-01 19:10:50 +1100

    Validation for login form  using Joi library done

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,29 +1,73 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
+import Input from "./common/input";
 class LoginForm extends Component {
-//creating a ref object
-username = React.createRef();
+  state = {
+    account: { username: "", password: "" },
+    errors: {},
+  };
 
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
     //call the server
     console.log("Submitted");
   };
 
-//i have to delete the ref object and remove from input field of username.
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
+  //i have to delete the ref object and remove from input field of username.
 
   render() {
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+            error={errors.username}
+          />
           <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input autoFocus ref={this.username} id="username" type="text" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <Input
+              name="password"
+              value={account.password}
+              label="Password"
+              onChange={this.handleChange}
+              error={errors.password}
+            />
           </div>
           <button className="btn btn-primary">Submit</button>
         </form>
